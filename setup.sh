c6e7989604ccd5091c178a19e21b08dffc7bfc76
TJ Barber <me@tjbarber.org>
2023-09-05 16:03:00 -0500

    creates symlinks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-ln -s ~/.alacritty.yml .alacritty.yml
-ln -s ~/.fonts .fonts
-ln -s ~/.config/nvim .config/nvim
+rm -f -- ~/.alacritty.yml
+rm -f -- ~/.fonts
+rm -f -- ~/.config/nvim
+ln -s "$(pwd)/.alacritty.yml" ~/.alacritty.yml
+ln -s "$(pwd)/.fonts" ~/.fonts
+ln -s "$(pwd)/.config/nvim" ~/.config/nvim
