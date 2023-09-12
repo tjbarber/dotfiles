30d8f19a633e62e6321ce7adce2e70036255bab6
TJ Barber <me@tjbarber.org>
2023-09-12 10:55:54 -0500

    adds copy path func

--- .config/nvim/lua/tjthedev/remap.lua
+++ .config/nvim/lua/tjthedev/remap.lua
@@ -1,3 +1,9 @@
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+vim.keymap.set('n', '<leader>cp', function ()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg("@", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end)
