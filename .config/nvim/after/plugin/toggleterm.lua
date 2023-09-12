43aca9f238ef40ea6d81eba4cff8da92acb172bf
TJ Barber <me@tjbarber.org>
2023-09-12 11:46:57 -0500

    adds screenshot keybinding on i3 and mapping to normal on toggleterm

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -1,3 +1,11 @@
 require("toggleterm").setup({
 	open_mapping = [[<C-Space>]]
 })
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
