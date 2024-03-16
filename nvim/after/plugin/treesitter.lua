d86f1984fe6aa5edb047bf55ac98029a167ed2d9
TJ Barber <me@tjbarber.org>
2024-03-16 13:56:33 -0500

    use html as the treesitter parser for blade files

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -9,7 +9,7 @@ require'nvim-treesitter.configs'.setup {
 	  "javascript",
 	  "ruby",
 	  "typescript",
-    "php",
+	  "php",
   },
 
   autotag = {
@@ -56,18 +56,8 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.blade = {
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = {"src/parser.c"},
-    branch = "main",
-  },
-  filetype = "blade"
-}
-
 vim.filetype.add({
   pattern = {
-    ['.*%.blade%.php'] = 'blade',
+    ['.*%.blade%.php'] = 'html',
   },
 })
