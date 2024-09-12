9d11bd1853f480dbd9c6e040ed10244c13779216
TJ Barber <me@tjbarber.org>
2024-09-12 18:56:23 -0400

    conform

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,9 +1,9 @@
--- require('conform').setup({
--- 	formatters_by_ft = {
--- 		php = { 'blade-formatter' }
--- 	},
--- 	format_on_save = {
--- 		timeout_ms = 1000,
--- 		lsp_format = "fallback",
--- 	},
--- })
+require('conform').setup({
+	formatters_by_ft = {
+		blade = { 'blade-formatter' }
+	},
+	format_on_save = {
+		timeout_ms = 1000,
+		lsp_format = "fallback",
+	},
+})
