4632d744f372421c1dd5398b7bd6ac83a5a6071e
TJ Barber <me@tjbarber.org>
2024-09-25 10:38:26 -0500

    fixes fugative

--- nvim/lua/tjthedev/plugins/fugative.lua
+++
@@ -1,9 +0,0 @@
--- vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
---
--- vim.api.nvim_create_user_command(
---   'Browse',
---   function (opts)
---     vim.fn.system { 'xdg-open', opts.fargs[1] }
---   end,
---   { nargs = 1 }
--- )
--- nvim/lua/tjthedev/plugins/vim-fugative.lua
+++ nvim/lua/tjthedev/plugins/vim-fugative.lua
@@ -1,3 +1,18 @@
 -- Git wrapper
 
-return { 'tpope/vim-fugitive' }
+return {
+	'tpope/vim-fugitive',
+	keys = {
+		{ '<leader>gs', vim.cmd.Git },
+	},
+	lazy = false,
+	config = function()
+		vim.api.nvim_create_user_command(
+			'Browse',
+			function(opts)
+				vim.fn.system { 'xdg-open', opts.fargs[1] }
+			end,
+			{ nargs = 1 }
+		)
+	end
+}
