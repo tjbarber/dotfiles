ddba47e80e2c5e3c6427f5a099d74c6f1749a8aa
TJ Barber <me@tjbarber.org>
2025-03-12 09:55:30 -0500

    conform uses prettier

--- nvim/lua/tjthedev/plugins/conform.lua
+++ nvim/lua/tjthedev/plugins/conform.lua
@@ -2,10 +2,21 @@
 
 return {
 	'stevearc/conform.nvim',
-	opts = {
-		format_on_save = {
-			timeout_ms = 1000,
-			lsp_format = "fallback",
-		},
-	}
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				lua = { "stylua" },
+				ruby = { "rubocop" }
+			},
+		})
+	end
 }
