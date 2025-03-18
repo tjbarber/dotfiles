76b527527ffe6dd6c3e99f3a4085619249fa2dc3
TJ Barber <me@tjbarber.org>
2025-03-18 15:57:23 -0500

    mini-statusline improvements

--- nvim/lua/tjthedev/plugins/mini-statusline.lua
+++ nvim/lua/tjthedev/plugins/mini-statusline.lua
@@ -8,9 +8,23 @@ return {
 
 		require('mini.statusline').setup {
 			content = {
-				active = noirbuddy_statusline.active,
+				active = function()
+					local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+					local oil_path      = require("oil").get_current_dir() or ""
+					local display_path  = vim.fn.fnamemodify(oil_path, ":~")
+
+					if vim.bo.filetype == 'oil' then
+						return MiniStatusline.combine_groups({
+							{ hl = mode_hl,                 strings = { mode } },
+							{ hl = 'MiniStatuslineDevinfo', strings = { display_path } },
+							'%<',
+						})
+					end
+
+					return noirbuddy_statusline.active()
+				end,
 				inactive = noirbuddy_statusline.inactive,
 			},
 		}
-	end,
+	end
 }
