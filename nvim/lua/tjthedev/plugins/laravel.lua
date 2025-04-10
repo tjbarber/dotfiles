6f184c905725217b82cc0d74c885364742aebc30
TJ Barber <me@tjbarber.org>
2025-04-10 15:28:12 -0500

    update laravel.lua

--- nvim/lua/tjthedev/plugins/laravel.lua
+++ nvim/lua/tjthedev/plugins/laravel.lua
@@ -1,25 +1,29 @@
 -- Laravel forever!
 
 return {
-	'adalessa/laravel.nvim',
+	"adalessa/laravel.nvim",
 	dependencies = {
-		'nvim-telescope/telescope.nvim',
-		'tpope/vim-dotenv',
-		'MunifTanjim/nui.nvim',
-		'nvimtools/none-ls.nvim',
-		'jwalton512/vim-blade'
+		"nvim-telescope/telescope.nvim",
+		"tpope/vim-dotenv",
+		"MunifTanjim/nui.nvim",
+		"nvimtools/none-ls.nvim",
+		"kevinhwang91/promise-async",
+		"jwalton512/vim-blade",
 	},
-	cmd = { 'Sail', 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
+	cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
 	keys = {
-		{ '<leader>la', ':Laravel artisan<cr>' },
-		{ '<leader>lr', ':Laravel routes<cr>' },
-		{ '<leader>lm', ':Laravel related<cr>' },
-		{ '<leader>lg', function()
-			local run = require 'laravel.run'
-			run('artisan', { 'ide-helper:generate' }, {})
-			run('artisan', { 'ide-helper:models' }, {})
-		end },
+		{ "<leader>la", ":Laravel artisan<cr>" },
+		{ "<leader>lr", ":Laravel routes<cr>" },
+		{ "<leader>lm", ":Laravel related<cr>" },
+		{
+			"<leader>lg",
+			function()
+				local run = require("laravel.run")
+				run("artisan", { "ide-helper:generate" }, {})
+				run("artisan", { "ide-helper:models" }, {})
+			end,
+		},
 	},
-	event = { 'VeryLazy' },
+	event = { "VeryLazy" },
 	config = true,
 }
