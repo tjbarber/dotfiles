e5728afb063980d256b077a1299d0c0486e97f75
TJ Barber <me@tjbarber.org>
2025-12-31 23:27:45 -0800

    goodbye i3

--- linux/i3/status_command.sh
+++
@@ -1,9 +0,0 @@
-while :
-do
-  date_formatted=$(date "+%a %F %H:%M")
-  current_volume="$(pamixer --get-volume-human) 🔊"
-
-  echo $current_volume $battery_status $date_formatted " "
-  sleep 5
-done
-
